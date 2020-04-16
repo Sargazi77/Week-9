86ff64fe4b9474056448a69f8b09bab59c7027a3
Your Name <gfdgd@gfd.com>
2020-04-16 02:37:51 -0500

    Student Sign in with Componants

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <NewSudentForm v-on:student-added="newStudentAdded"></NewSudentForm>
+
+  <studentTable v-bind:students="students"
+  v-on:student-present="studentArrivedOrLeft"
+  v-on:delete-student="studentDeleted">
+  </StudentTable>
+
+  <studentMessage v-bind:message="message" v-bind:name="name"></studentMessage>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NewSudentForm from './components/NewSudentForm.vue'
+import studentMessage from './components/studentMessage.vue'
+import studentTable from './components/studentTable.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NewSudentForm,
+    studentMessage,
+    studentTable
+  },
+  data() {
+    return{
+      students: [],
+      message:'',
+      name:''
+      
+    }
+  },
+  methods: {
+    newStudentAdded(student) {
+      this.students.push(student)
+      this.students.sort(function (s1, s2){
+        return s1.name.toLowerCase() < s2.name.toLowerCase() ? -1: 1
+      })
+    }, 
+    studentArrivedOrLeft(student) {
+      this.message = student.present ? 'welcome, ' : 'Goodbye, '
+      this.name= student.name
+    
+  },
+  studentDeleted(student) {
+    this.students = this.students.filter( s => s != student )
+  }
+    
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
